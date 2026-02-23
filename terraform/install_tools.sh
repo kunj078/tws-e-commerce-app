5104530e8884ce29679600fb0826dc323daf59fb
Kunj_Khimani_ <117498722+kunj078@users.noreply.github.com>
2026-02-23 14:04:46 +0530

    Update Jenkins key URL in install_tools.sh

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -6,7 +6,7 @@ sudo apt install -y fontconfig openjdk-17-jre
 
 # Jenkins installation
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
